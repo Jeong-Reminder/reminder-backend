e946c6f4850d0ce2c7d4e49584b8b13f63cadded
josephuk77 <josephuk77@gmail.com>
2024-06-16 19:01:05 +0900

    feat : #16 팀원신청글 Repository생성, 어노테이션 추가
    팀원신청글의 Repository를 생성하고 모든 Repository에 어노테이션을 추가하여 SpringBean으로 관리될 수 있게 변경

--- src/main/java/com/example/backend/model/repository/member/ProfileRepository.java
+++ src/main/java/com/example/backend/model/repository/member/ProfileRepository.java
@@ -2,6 +2,8 @@ package com.example.backend.model.repository.member;
 
 import com.example.backend.model.entity.member.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 }
