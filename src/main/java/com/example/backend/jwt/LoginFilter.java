420c83f81d187710ed2e78547354d603ae4bae92
josephuk77 <josephuk77@gmail.com>
2024-06-22 23:00:14 +0900

    feat : #27 Access 토큰 필터 : JWTFilter
    리프레쉬 토큰 추가로 로그인 필터 코드 수정

--- src/main/java/com/example/backend/jwt/LoginFilter.java
+++ src/main/java/com/example/backend/jwt/LoginFilter.java
@@ -5,12 +5,14 @@ import com.example.backend.model.entity.member.Member;
 import com.example.backend.model.entity.member.UserRole;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -51,14 +53,28 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         Long memberId = customMemberDetails.getMemberId();
 
-        String token = jwtUtil.createJwt(studentId, userRole, 60*60*24*30L, memberId);
+        String access = jwtUtil.createJwt("access",studentId, userRole, 60*60*24*7L, memberId);
+        String refresh = jwtUtil.createJwt("refresh",studentId, userRole, 60*60*24*30L, memberId);
 
-        response.addHeader("Authorization", "Bearer " + token);
+        response.setHeader("access", access);
+        response.addCookie(createCookie("refresh", refresh));
+        response.setStatus(HttpStatus.OK.value());
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
 
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+        //cookie.setSecure(true);
+        //cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
