724d34b4f5ad6cb0004aa264603e71ff9e9f8382
josephuk77 <josephuk77@gmail.com>
2024-06-28 01:00:43 +0900

    fix : #40 security수정

--- src/main/java/com/example/backend/config/SecurityConfig.java
+++ src/main/java/com/example/backend/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/login","/","/api/v1/member/signup").permitAll()
                         .requestMatchers("/api/v1/reissue").permitAll()
                         .anyRequest().authenticated());
         //JWTFilter 등록
